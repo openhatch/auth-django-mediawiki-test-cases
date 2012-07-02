0747a1bd252957b3af74feaa396fae502888b34c
Asheesh Laroia <asheesh@asheesh.org>
2012-07-02 14:58:59 -0600

    Test that logging out successfully does not show you the annoying IP address nonsense

--- tests.py
+++ tests.py
@@ -7,6 +7,9 @@ import ConfigParser
 config = ConfigParser.SafeConfigParser()
 config.read('config.ini')
 
+### Nab cookies:
+### b._ua_handlers['_cookies'].cookiejar
+
 class LoginGenerallyWorks(unittest2.TestCase):
     def test_login(self):
         # First, log in
@@ -49,13 +52,38 @@ class LoginGenerallyWorks(unittest2.TestCase):
 
     def test_wiki_logout_shows_login_link(self):
         b = self.test_wiki_username_follows()
+        self.assertTrue('zomg_oh_wiki__session' in get_cookie_names(b))
+        self.assertTrue('sessionid' in get_cookie_names(b))
         # Click the logout link
         b.open('https://openhatch.org/wiki/Special:UserLogout').read()
+        # Logging out should:
+        # - remove the Django-created sessionid cookie, and the wiki-specific
+        #   session cookie
+        b.open('https://openhatch.org/w/extensions/AuthDjango/destroy_session.php').read()
+        self.assertFalse('sessionid' in get_cookie_names(b))
+        #self.assertFalse('zomg_oh_wiki__session' in get_cookie_names(b))
+
         # Assert that the user is encouraged to log in
         data = b.open('https://openhatch.org/wiki/').read()
+        ### Verify that just visiting the wiki does not cause you to get
+        ### a wiki session ID cookie.
+        #self.assertFalse('zomg_oh_wiki__session' in get_cookie_names(b))
         self.assertFalse('The user page for the IP address you are editing as'
                          in data)
         self.assertTrue('/w/index.php?title=Special:UserLogin' in data)
+
+def get_cookies(b):
+    cookies = b._ua_handlers['_cookies'].cookiejar
+    cookies_we_care_about = cookies._cookies['.openhatch.org']['/']
+    cookies_we_care_about.update(
+        cookies._cookies['openhatch.org']['/'])
+    return cookies_we_care_about
+#    cookie_names = cookies_we_care_about.keys()
+#    return cookie_names
+
+def get_cookie_names(b):
+    cookies = get_cookies(b)
+    return cookies.keys()
 
 if __name__ == '__main__':
     unittest2.main()
