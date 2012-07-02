5e0c77c1d5cddfd08c7acfb49725e83fe943f395
Asheesh Laroia <asheesh@asheesh.org>
2012-07-02 11:59:25 -0600

    Add a login test that passes

--- tests.py
+++ tests.py
@@ -1,13 +1,26 @@
 #!/usr/bin/python
 import unittest2
+import mechanize
 
 import ConfigParser
 
-config = ConfigParser.SafeConfigParser().read('config.ini')
+config = ConfigParser.SafeConfigParser()
+config.read('config.ini')
 
 class LoginGenerallyWorks(unittest2.TestCase):
-    def test(self):
-        pass
+    def test_login(self):
+        # First, log in
+        b = mechanize.Browser()
+        b.open('https://openhatch.org/account/login/old')
+        b.select_form(nr=0)
+        username = config.get('login', 'username')
+        b['username'] = username
+        b['password'] = config.get('login', 'password')
+        b.submit()
+        # Then, go visit the front page and ensure the username appears
+        response = b.open('https://openhatch.org/')
+        data = response.read()
+        self.assertTrue(username in data)
 
 if __name__ == '__main__':
     unittest2.main()
